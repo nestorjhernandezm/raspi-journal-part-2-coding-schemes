193d6d0f5f4c4380f9ea8e8cad7c10f189b9a86b
Chres Sørensen <chres.soerensen@gmail.com>
2016-06-03 20:13:25 +0200

    testbed

--- upload_rasp_config.sh
+++ upload_rasp_config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-zip -r rasp_config rasp_config
-scp rasp_config.zip js3:/afs/ies.auc.dk/project/TuneSCode/public_html/
-rm rasp_config.zip
+cd rasp_config
+zip -r rasp_config *
+#zip -r rasp_config rasp_config
+#scp rasp_config.zip js3:/afs/ies.auc.dk/project/TuneSCode/public_html/
+#rm rasp_config.zip
