9b8eea19d0c267862c3969363787237642290610
Chres Sørensen <chres.soerensen@gmail.com>
2016-06-05 13:31:37 +0200

    Updating cross compiling

--- upload_rasp_config.sh
+++ upload_rasp_config.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 zip -r rasp_config rasp_config
-scp rasp_config.zip js3:/afs/ies.auc.dk/project/TuneSCode/public_html/
+scp rasp_config.zip js3:/afs/ies.auc.dk/project/TuneSCode/public_html/raspi/
 rm rasp_config.zip
